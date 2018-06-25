46d9ebd2e973dc17ddff28c873b2ae193068f6d6
Valtýr Örn Kjartansson <valtyr@gmail.com>
2018-06-25 22:15:28 +0000

    kinda working

--- lib/chitchat.ex
+++ lib/chitchat.ex
@@ -11,25 +11,39 @@ defmodule Chitchat do
     end
   end
 
+  def pipe(is_last) do
+    case is_last do
+      false -> "├──"
+      true -> "└──"
+    end
+  end
+
   def dir_list(dir \\ ".") do
     depth = length Path.split(dir)
-    padding = String.duplicate("  ", depth - 1)
+    padding = String.duplicate("   ", depth - 1)
 
     items = Enum.filter(File.ls!(dir), fn item -> hidden?(item) end)
 
     files = Enum.filter(items, fn item -> item_is(:file, item, dir) end)
     folders = Enum.filter(items, fn item -> item_is(:folder, item, dir) end)
 
-    for file <- files do
-      IO.puts padding <> file
+    for {file, i} <- Enum.with_index(files) do
+      is_last = length(files) - 1 == i
+      IO.puts padding <> pipe(is_last) <> file
     end
 
-    for folder <- folders do
-      IO.puts padding <> folder
+    for {folder, i} <- Enum.with_index(folders) do
+      is_last = length(folders) - 1 == i
+      IO.puts padding <> pipe(is_last) <> folder
       dir_list Path.join(dir, folder)
     end
   end
+
+  def print_tree do
+    IO.puts "."
+    dir_list()
+  end
 end
 
-Chitchat.dir_list
+Chitchat.print_tree
 
